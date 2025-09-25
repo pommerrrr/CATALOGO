cf66fde7796775e51391a001db5b4be63c2ab235
pommerrrr <119147432+pommerrrr@users.noreply.github.com>
2025-09-24 23:36:15 -0300

    Update login.ts

--- api/ml/login.ts
+++ api/ml/login.ts
@@ -4,7 +4,7 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const appId = process.env.ML_APP_ID!;
   const redirect = process.env.ML_REDIRECT_URI!;
-  const authUrl = `https://auth.mercadolibre.com/authorization?response_type=code&client_id=${appId}&redirect_uri=${encodeURIComponent(redirect)}`;
-  // (alternativo BR: https://auth.mercadolivre.com.br/authorization)
+  // Use o host BR:
+  const authUrl = `https://auth.mercadolivre.com.br/authorization?response_type=code&client_id=${appId}&redirect_uri=${encodeURIComponent(redirect)}`;
   res.status(302).setHeader("Location", authUrl).end();
 }
